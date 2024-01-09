085c665865f3ea41ed539a288f31cac8b9fee4bf
ErtugrulArac <aracertugrul407@gmail.com>
2024-01-09 18:22:17 +0300

    arkaplan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className='bg-black'>
           <main className='min-h-screen max-sm:py-3 bg-black flex items-center'>{children}</main>
       </body>
     </html>
