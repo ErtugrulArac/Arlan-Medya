90021799b2d60ad70cf20597241d5595ac6b90ec
ErtugrulArac <aracertugrul407@gmail.com>
2024-01-10 13:55:01 +0300

    güncelleme

---
+++ src/app/hakkımda/hakkımda.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const hakkımda = () => {
+    return (
+        <div>
+            <h1>ERZURUM REKLAM AJANSI</h1>
+            <p>Erzurum'da reklam ajansı olarak faaliyet gösteren Arlan Medya, markanızı güçlendirmek, hedef kitlenize ulaşmak ve etkileşimi artırmak için yaratıcı ve özgün çözümler sunmaktadır. Profesyonel ekip ve teknolojik alt yapı ile müşteri memnuniyetini ön planda tutuyoruz.</p>
+
+            <h2>ERZURUM WEB SİTE</h2>
+            <p>Web siteniz, dijital varlığınızın temelidir. Erzurum web site tasarımı konusunda Arlan Medya, modern ve kullanıcı dostu çözümler sunarak işletmenizin çevrimiçi başarısını artırmaktadır. Mobil uyumlu, hızlı ve estetik web siteleri ile markanızın çevrimiçi varlığını güçlendiriyoruz.</p>
+
+            <h3>ERZURUM ARLAN MEDYA</h3>
+            <p>Arlan Medya, Erzurum'da reklam, medya ve dijital pazarlama alanında uzmanlaşmış bir ajans olarak müşterilerine yenilikçi çözümler sunmaktadır. Sizinle birlikte büyümek için buradayız. Kreatif kampanyalar, etkili medya stratejileri ve güçlü dijital varlık ile markanızı ön plana çıkarıyoruz.</p>
+
+            <h4>ERZURUM YAZILIM</h4>
+            <h4>Arlan Medya: Erzurum'un Öncü Reklam ve Medya Ajansı</h4>
+            <p>Erzurum'da faaliyet gösteren Arlan Medya, reklam ve medya alanında uzmanlaşmış bir ajans olarak markanıza güç katmaya hazır. Sektördeki deneyimimiz ve profesyonel ekibimizle, dijital varlığınızı güçlendirmek ve hedef kitlenize ulaşmak için etkili çözümler sunuyoruz.</p>
+            <p>Arlan Medya olarak, web tasarımından dijital pazarlamaya, yazılım çözümlerinden sosyal medya yönetimine kadar geniş bir yelpazede hizmet sunmaktayız. Müşteri odaklı yaklaşımımız ve yenilikçi stratejilerimizle, işletmenizin online başarısını artırmak için yanınızdayız.</p>
+            <p>Markanızın öne çıkmasını istiyorsanız, Arlan Medya'nın uzmanlığından faydalanın. Etkili reklam kampanyaları, özgün web tasarımları ve etkileşimli sosyal medya stratejileri ile markanızı güçlendirelim. Sizinle birlikte, dijital dünyada etkileyici bir varlık oluşturmak için adım atalım.</p>
+            <p>İşinizi büyütmek, müşteri tabanınızı genişletmek ve rekabet avantajı elde etmek için Arlan Medya'yı tercih edin. Erzurum'un öncü reklam ajansı olarak, sizinle birlikte projeler üretmek ve başarıya ulaşmak için sabırsızlanıyoruz. Bize ulaşın ve markanızı dijital dünyada öne çıkarmanın keyfini yaşayın.</p>
+
+            <h5>ERZURUM MEDYA AJANSI</h5>
+            <p>Erzurum medya ajansı olarak Arlan Medya, reklamdan sosyal medya yönetimine kadar geniş bir yelpazede medya hizmetleri sunmaktadır. Markanızın görünürlüğünü artırmak için buradayız. Yaratıcı içerik üretimi, etkileşimli sosyal medya kampanyaları ve reklam stratejileri ile markanızı öne çıkarıyoruz.</p>
+        </div>
+    )
+}
+
+export default hakkımda
---
+++ src/app/iletisim/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='w-[100%]'>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12242.223970457842!2d41.2707265!3d39.9065722!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2628a79910cc9b5f%3A0xd45613ce0f4c8482!2sArlan%20Medya%20-%20Dijital%20Reklam%20Ajansı!5e0!3m2!1str!2str!4v1704882172113!5m2!1str!2str" width="100%" height="800" loading="lazy"></iframe>
+        </div>
+    )
+}
+
+export default page
